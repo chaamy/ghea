cfa4e25b4e9f664d9ab95d66084fce113ed5d9d7
chaamy <247006111003@student.unsil.ac.id>
2026-02-11 02:38:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const sections = document.querySelectorAll("section");
 
-window.addEventListener("scroll", () => {
-    sections.forEach(section => {
-        const posisi = section.getBoundingClientRect().top;
-        const tinggiLayar = window.innerHeight;
-
-        if (posisi < tinggiLayar - 100) {
-            section.classList.add("show");
-        }
-    });
-});
